f3efb9d16416f814231d07b7d7e8a80179d3e71c
antolow <98319496+antolow@users.noreply.github.com>
2023-03-10 18:08:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let disp = document.querySelector("#display");
+let keys = document.querySelectorAll('.key')
+let keyboard = document.querySelector('.keyboard')
+let result = document.querySelector('#result')
+let reset = document.querySelector('#reset')
+let exp = ''
+// exp + btn.textContent
+
+for (let key of keys) {
+    key.addEventListener('click', function () {
+        exp = exp + key.textContent
+        disp.value = exp
+
+    })
+
+
+}
+
+result.addEventListener('click', function () {
+    let exp = eval(disp.value);
+    console.log(exp);
+    disp.value = exp
+})
+
+reset.addEventListener('click', function(){
+    
+})
